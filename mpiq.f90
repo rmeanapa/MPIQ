866f3fac291207381043c824b9f9d2f863022889
Ruben Meana-Paneda <rubenmepa@gmail.com>
2023-06-13 08:24:34 -0400

    Update MPIQ Fortran code

--- mpiq.f90
+++ mpiq.f90
@@ -1,37 +1,34 @@
-program pruns
+program mpiq 
+
 !===================================================================
-!  MPI FOTRAN code to run several jobs in parallel
+!  MPIQ: MPI Fortran code to wrap and run several jobs in parallel
 !===================================================================
-! Author:  Ruben Meana-Paneda
-! Date:    July 10, 2014
-! Updated: December 28, 2015
-! Version: 3.0
 !
 implicit none
 include 'mpif.h'
-character*80,allocatable :: fname(:)
-character*80 :: proc_name,scrdir
-character*1000,allocatable :: runcode(:)
-integer, allocatable :: t1(:),t2(:)
-double precision :: ti,tf
-integer :: i,irank,ierror,nproc,ntasks,ierr,leng,count_rate
+character(len=80),allocatable   :: fname(:)
+character(len=80)               :: proc_name,scrdir
+character(len=1000),allocatable :: runcode(:)
+integer, allocatable            :: t1(:),t2(:)
+real                            :: ti,tf
+integer                         :: i,irank,ierror,nproc,ntasks,ierr,leng,count_rate
 !
 call MPI_INIT(ierror)
 call MPI_COMM_SIZE (MPI_COMM_WORLD, nproc, ierror)
 call MPI_COMM_RANK (MPI_COMM_WORLD, irank, ierror)
 call MPI_GET_PROCESSOR_NAME(proc_name,leng, ierror)
-ti=MPI_Wtime()
+ti = MPI_Wtime()
 !
-if(irank.eq.0)then
- open(unit=20,file='jobslist.txt')                                      ! Open file with the number and the names of the input files
+if( irank.eq.0 )then
+ open(unit=20,file='jobslist.txt') ! Open file with the number and the names of the input files
  read(20,*) ntasks
- if (nproc.gt.ntasks) then                                              ! Check number of processors and number of tasks 
+ if( nproc.gt.ntasks ) then        ! Check number of processors and number of tasks 
    write(6,*) "Error: The number of requested nodes is greater than the number of jobs."
    write(6,*) "The number of jobs must be equal or greater than the number of nodes."
    call MPI_ABORT(MPI_COMM_WORLD,ierror)
    stop
- endif
-endif
+ end if
+end if
 call mpi_bcast(ntasks,1,mpi_integer,0,mpi_comm_world,ierr)
 allocate(runcode(ntasks),t1(ntasks),t2(ntasks),fname(ntasks))
 if(irank.eq.0)then
@@ -53,14 +50,14 @@ if(irank.eq.0)then
 !   & "; rm -f *scratchfiles* "// &                                                        ! Remove scratch files if needed
 !   & "; cd .."                                                                            ! Change to home work directory
 
- enddo
+ end do
  close(20)                                                             ! Close file with the number and input file names
-endif
+end if
 call mpi_bcast(fname,80*ntasks,mpi_character,0,mpi_comm_world,ierr)
 call mpi_bcast(runcode,1000*ntasks,mpi_character,0,mpi_comm_world,ierr)
-scrdir     = "mkdir -p /scratch/meanapan"                              ! Scratch directory if needed
+scrdir = "mkdir -p /scratch/rmeanapa"                                  ! Scratch directory if needed
 !
-do i=irank+1,ntasks,nproc
+do i = irank+1,ntasks,nproc
  call system(scrdir)                                                   ! Create scratch directory if needed
  call system_clock(t1(i),count_rate)                                   ! Start time
  call system(runcode(i))                                               ! Run code 
@@ -68,7 +65,7 @@ do i=irank+1,ntasks,nproc
  write(6,'(A4,A20,A13,A10,A7,I4,A14,f20.3,A9)') "Job ",fname(i)," executed in ",&
  &proc_name," irank ",irank," completed in ",real(t2(i)-t1(i))/real(count_rate), " seconds."
 enddo
-tf=MPI_Wtime()
+tf = MPI_Wtime()
 write(6,'(A14,A10,f20.3,A9)') "Total time in ",proc_name,tf-ti," seconds."
 call MPI_FINALIZE(ierror)
-end
+end program mpiq
